97ac9e3c45b7750678675db13c2e43ae64b2bcd4
Eka Jacinda Rahmawati <rikkayuki68@gmail.com>
2022-10-13 17:50:55 +0700

    push web

---
+++ kal.js
@@ -0,0 +1,31 @@
+   
+   
+   function tambah(){
+   let a = document.getElementById('angka1').value;
+   let b = document.getElementById('angka2').value;
+   let c = a+b; 
+   alert("hasilnya : "+c);
+   console.log(c);
+   }
+   function kurang(){
+      let a = document.getElementById('angka1').value;
+      let b = document.getElementById('angka2').value;
+      let c = a-b;
+      alert("hasilnya : "+c);
+      console.log(c);
+   }
+   function kali(){
+      let a = document.getElementById('angka1').value;
+      let b = document.getElementById('angka2').value;
+      let c = a*b;
+      alert("hasilnya : "+c);
+      console.log(c);
+   }
+   function bagi(){
+      let a = document.getElementById('angka1').value;
+      let b = document.getElementById('angka2').value;
+      let c = a/b;
+      alert("hasilnya : "+c);
+      console.log(c);
+   }
+   
